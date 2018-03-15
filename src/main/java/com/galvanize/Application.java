60576def552d637d33f24608c568e293ca542873
Nicholas Fallon <nickfallonwastaken@gmail.com>
2018-03-15 01:33:37 -0600

    Testing

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,9 +1,60 @@
 package com.galvanize;
 
+//class BankAccount {
+//
+//    private long balance = 0;
+//
+//    void deposit(long cents) {
+//        balance += cents;
+//    }
+//
+//    void withdraw(long cents) {
+//        balance -= cents;
+//    }
+//
+//    private long balance() {
+//        return balance;
+//    }
+//
+//    private boolean isOverDrawn() {
+//        if (balance < 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "BankAccount{" +
+//                "balance=" + balance +
+//                '}';
+//    }
+//}
+
+//public class Picture {
+//    private boolean framed;
+//    private int height;
+//    private int width;
+//
+//    Picture(int height, int width) {
+//        this(false, height, width);
+//    }
+//
+//    Picture(boolean framed, int height, int width) {
+//        this.framed = framed;
+//        this.height = height;
+//        this.width = width;
+//    }
+//}
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-    }
 
+        for (int i = 2; i <= 6 ; i+=2) {
+            System.out.println(i);
+        }
+
+    }
 }
---
+++ src/main/java/com/galvanize/BankAccount.java
@@ -0,0 +1,26 @@
+package com.galvanize;
+
+//public class BankAccount {
+//
+//    private long balance = 0;
+//
+//    void deposit(long cents) {
+//        balance += cents;
+//    }
+//
+//    void withdraw(long cents) {
+//        balance -= cents;
+//    }
+//
+//    private long balance() {
+//        return balance;
+//    }
+//
+//    private boolean isOverDrawn() {
+//        if (balance < 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//}
